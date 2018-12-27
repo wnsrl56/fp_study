26a439c6146701d3fe1bfa546eda8b566b9c6738
Jun <wnsrl56@gmail.com>
2018-12-27 10:31:02 +0900

    added fp folder and files

---
+++ fp/chapter01.test.js
@@ -0,0 +1,19 @@
+const [filter, map, filter_imperative, map_imperative] = require('./chapter01');
+
+const users = [
+  { name: 'TN', age: 76 },
+  { name: 'KG', age: 34 },
+  { name: 'DK', age: 54 },
+  { name: 'FD', age: 64 },
+  { name: 'GF', age: 42 },
+  { name: 'FE', age: 45 },
+  { name: 'FD', age: 35 }
+];
+
+test("filter function's result is Equal to filter_imperative.", () => {
+  expect(filter(users, v => v.age < 35)).toEqual(filter_imperative(users, 35));
+});
+
+test("map's result is Equal to map_impreative.", () => {
+  expect(map([1, 2, 3], v => v * 2)).toEqual(map_imperative([1, 2, 3]));
+});
